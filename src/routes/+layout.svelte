ccbf903fa166ddf6c3c84ae28a64f62c30828246
tglide <26071571+TGlide@users.noreply.github.com>
2023-03-21 11:23:38 +0000

    :recycle: migrate from plausible to vercel analytics'

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import Footer from '$components/Footer.svelte';
 	import '$styles';
+
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <main>
